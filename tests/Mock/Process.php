4fdd3db5f9f3f036e8fae2445be9d1de58d2cc1e
jmsche <contact@jmsche.fr>
2021-10-22 14:32:18 +0200

    Fix last missing return types

--- tests/Mock/Process.php
+++ tests/Mock/Process.php
@@ -37,26 +37,17 @@ class Process extends BaseProcess
         return 0;
     }
 
-    /**
-     * @return mixed
-     */
-    public function isSuccessful()
+    public function isSuccessful(): bool
     {
         return $this->success;
     }
 
-    /**
-     * @return string
-     */
-    public function getOutput()
+    public function getOutput(): string
     {
         return 'output';
     }
 
-    /**
-     * @return string
-     */
-    public function getErrorOutput()
+    public function getErrorOutput(): string
     {
         return 'errormsg';
     }
